abfc87a8a8298058a378a725231a0ce3e67639c8
Brian Norman <brian@bnorm.com>
2024-03-15 14:11:13 -0500

    Move section code to different package

--- librettist-core/src/commonMain/kotlin/dev/bnorm/librettist/show/SlideSection.kt
+++ librettist-core/src/commonMain/kotlin/dev/bnorm/librettist/show/SlideSection.kt
@@ -1,23 +1,24 @@
-package dev.bnorm.librettist.section
+package dev.bnorm.librettist.show
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.compositionLocalOf
-import dev.bnorm.librettist.show.ShowBuilder
-import dev.bnorm.librettist.show.ShowBuilderDsl
-import dev.bnorm.librettist.show.SlideContent
 
 @Immutable
 class SlideSection(
-    val header: @Composable () -> Unit,
+    val title: @Composable () -> Unit,
 ) {
     companion object {
         val Empty = SlideSection {}
+
+        val header: @Composable () -> Unit
+            @Composable
+            get() = LocalSlideSection.current.title
     }
 }
 
-val LocalSlideSection = compositionLocalOf { SlideSection.Empty }
+private val LocalSlideSection = compositionLocalOf { SlideSection.Empty }
 
 @ShowBuilderDsl
 fun ShowBuilder.section(
