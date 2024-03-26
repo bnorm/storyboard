371604850bcc8a689d2c70f9fa5cdd876b02d01d
Brian Norman <brian@bnorm.com>
2024-03-26 08:36:15 -0500

    Deduplicate emissions from AnimationSequence

--- librettist-text/src/commonMain/kotlin/dev/bnorm/librettist/animation/AnimationSequence.kt
+++ librettist-text/src/commonMain/kotlin/dev/bnorm/librettist/animation/AnimationSequence.kt
@@ -1,12 +1,31 @@
 package dev.bnorm.librettist.animation
 
-// TODO hook into SeekableTransitionState and KeyframesSpec?
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+
 data class AnimationSequence<T>(
     val start: T,
     val end: T,
     val sequence: Sequence<T>,
-)
+) {
+    fun toList(): ImmutableList<T> {
+        return sequence.deduplicate().toImmutableList()
+    }
+}
 
 fun <T> startAnimation(start: T): AnimationSequence<T> {
     return AnimationSequence(start, start, sequenceOf(start))
+}
+
+fun <T> Sequence<T>.deduplicate(): Sequence<T> {
+    val upstream = this
+    return sequence {
+        var prev: Any? = Any()
+        for (it in upstream) {
+            if (it != prev) {
+                yield(it)
+            }
+            prev = it
+        }
+    }
 }
