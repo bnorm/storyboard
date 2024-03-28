1dda83aff5fe132108bb2f00359e559f67ba0870
Brian Norman <brian@bnorm.com>
2024-03-28 18:05:34 -0500

    Allow directly appending AnimationSequence

--- librettist-text/src/commonMain/kotlin/dev/bnorm/librettist/animation/AnimationSequence.kt
+++ librettist-text/src/commonMain/kotlin/dev/bnorm/librettist/animation/AnimationSequence.kt
@@ -17,6 +17,16 @@ fun <T> startAnimation(start: T): AnimationSequence<T> {
     return AnimationSequence(start, start, sequenceOf(start))
 }
 
+fun <T> AnimationSequence<T>.then(next: T): AnimationSequence<T> {
+    val sequence = sequence { yieldAll(sequence); yield(next) }
+    return copy(end = next, sequence = sequence)
+}
+
+fun <T> AnimationSequence<T>.then(next: AnimationSequence<T>): AnimationSequence<T> {
+    val sequence = sequence { yieldAll(sequence); yieldAll(next.sequence) }
+    return copy(end = next.end, sequence = sequence)
+}
+
 fun <T> Sequence<T>.deduplicate(): Sequence<T> {
     val upstream = this
     return sequence {
