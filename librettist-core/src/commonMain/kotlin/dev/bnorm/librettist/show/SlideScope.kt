9f57c17275db00db62fa1fed0866b56b4a7901fd
Brian Norman <brian@bnorm.com>
2024-04-30 13:08:09 -0500

    Support creating SlideScope from value

--- librettist-core/src/commonMain/kotlin/dev/bnorm/librettist/show/SlideScope.kt
+++ librettist-core/src/commonMain/kotlin/dev/bnorm/librettist/show/SlideScope.kt
@@ -2,7 +2,9 @@ package dev.bnorm.librettist.show
 
 import androidx.compose.animation.core.Transition
 import androidx.compose.animation.core.createChildTransition
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 
 // TODO rename to SlideBuilder and add ShowBuilderDsl?
 class SlideScope<out T>(
@@ -11,8 +13,12 @@ class SlideScope<out T>(
 )
 
 @Composable
-fun <T, R> SlideScope<T>.createChildScope(transform: (T) -> R): SlideScope<R> {
-    val transition = transition.createChildTransition { transform(it) }
-    return SlideScope(transition) // TODO remember?
+fun <T> SlideScope(value: T): SlideScope<T> {
+    return SlideScope(updateTransition(value))
 }
 
+@Composable
+fun <T, R> SlideScope<T>.createChildScope(transform: (T) -> R): SlideScope<R> {
+    val transition = transition.createChildTransition { transform(it) }
+    return remember(transition) { SlideScope(transition) }
+}
