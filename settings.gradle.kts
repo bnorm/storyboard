1511e5ea6466a49c0f3f6b49ada1599e384ce675
Brian Norman <brian@bnorm.com>
2024-07-07 11:33:13 -0500

    Work around for WASM crash

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,16 @@
 pluginManagement {
     repositories {
+        mavenCentral()
+        google()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
     }
 
     plugins {
-        kotlin("multiplatform") version "2.0.0"
-        kotlin("plugin.compose") version "2.0.0"
+        val kotlinVersion = "2.0.10-RC-510"
+
+        kotlin("multiplatform") version kotlinVersion
+        kotlin("plugin.compose") version kotlinVersion
         id("org.jetbrains.compose") version "1.7.0-alpha01"
     }
 
@@ -13,6 +18,7 @@ pluginManagement {
         repositories {
             mavenCentral()
             google()
+            maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
         }
     }
 }
