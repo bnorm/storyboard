8eeaff2d84fedcd73cbf4d7aa5697732ddb6aff8
Brian Norman <brian.norman@jetbrains.com>
2025-04-09 09:17:51 -0500

    Support splitting by TextTag for MagicText animations

--- storyboard-text/src/commonMain/kotlin/dev/bnorm/storyboard/text/TextTag.kt
+++ storyboard-text/src/commonMain/kotlin/dev/bnorm/storyboard/text/TextTag.kt
@@ -4,7 +4,11 @@ import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 
-data class TextTag(val id: String) {
+data class TextTag(
+    val id: String,
+    val description: String,
+    val category: String? = null,
+) {
     init {
         require(TAG_START !in id && TAG_END !in id) { "id cannot contain '$TAG_START' or '$TAG_END': $id" }
     }
@@ -46,7 +50,7 @@ data class TextTag(val id: String) {
                 offset = match.range.endInclusive + 1
 
                 // Build a list of tag ranges.
-                val tag = TextTag(match.groupValues[1])
+                val tag = TextTag(match.groupValues[1], "", null)
                 val last = starts.remove(tag)
                 if (last != null) {
                     val range = last..<(match.range.start - removed)
@@ -102,17 +106,55 @@ fun AnnotatedString.addStyleByTag(
         }
     }
 
+    // TODO(CMP-7955) this is actually a bug!
+    // Note: unfortunately, it seems that while styles are applied first to last,
+    // they are also applied start to end.
+    //
+    // Consider the following AnnotatedString, with the applied styles:
+    //              "Hello, World!"
+    // style1               |---|
+    // style2        |-----------|
+    //
+    // In this case (it seems) that while 'style2' was applied after 'style1',
+    // 'style1' will still be displayed because it starts at a later offset.
+    // It seems like there is some kind of running style,
+    // and new styles are folded in as they are encountered,
+    // without existing styles being reapplied.
+
     val newText = buildAnnotatedString {
+        // TODO optimize by only adding the text?
+        //  - would need to also add the TextTag annotations as well
+        //  - AnnotatedString.Builder.flatMapAnnotations might be the answer?!
+        //     - would need to do that as a last step...
         append(this@addStyleByTag)
 
         var last = 0
         for (range in ranges) {
-            if (range.start != last && untagged != null) addStyle(untagged, last, range.start)
-            if (tagged != null) addStyle(tagged, range.start, range.end)
+            if (range.start != last && untagged != null) {
+                // !!! Note: see the comment on 'newText' for why this is needed.
+                val styles = this@addStyleByTag.subSequence(last, range.start).spanStyles
+                for (subRange in styles) {
+                    addStyle(subRange.item + untagged, last + subRange.start, last + subRange.end)
+                }
+                addStyle(untagged, last, range.start)
+            }
+            if (tagged != null) {
+                // !!! Note: see the comment on 'newText' for why this is needed.
+                val styles = this@addStyleByTag.subSequence(range.start, range.end).spanStyles
+                for (subRange in styles) {
+                    addStyle(subRange.item + tagged, range.start + subRange.start, range.start + subRange.end)
+                }
+                addStyle(tagged, range.start, range.end)
+            }
             last = range.end
         }
 
         if (last != text.length && untagged != null) {
+            // !!! Note: see the comment on 'newText' for why this is needed.
+            val styles = this@addStyleByTag.subSequence(last, text.length).spanStyles
+            for (subRange in styles) {
+                addStyle(subRange.item + untagged, last + subRange.start, last + subRange.end)
+            }
             addStyle(untagged, last, text.length)
         }
     }
@@ -137,6 +179,8 @@ fun AnnotatedString.replaceAllByTag(
         if (replacement.isNotEmpty()) {
             builder.append(replacement)
         }
+        builder.addStringAnnotation(TextTag.TAG_NAME, tag.id, builder.length - replacement.length, builder.length)
+
         last = range.end
     }
 
@@ -145,4 +189,42 @@ fun AnnotatedString.replaceAllByTag(
     }
 
     return builder.toAnnotatedString()
+}
+
+fun AnnotatedString.splitByTags(): List<AnnotatedString> {
+    val ranges = getStringAnnotations(TextTag.TAG_NAME, 0, length)
+    val splits = buildSet {
+        for (range in ranges) {
+            add(range.start)
+            add(range.end)
+        }
+
+        var padding = true
+        text.forEachIndexed { index, char ->
+            if (padding && !char.isWhitespace()) {
+                add(index)
+                padding = false
+            }
+            if (char == '\n') {
+                add(index)
+                padding = true
+            }
+        }
+    }.sorted()
+
+    if (splits.isEmpty()) return listOf(this)
+
+    val str = this
+    val split = buildList {
+        var last = 0
+        for (next in splits) {
+            if (next <= last) continue
+            add(str.subSequence(last, next))
+            last = next
+        }
+        if (last < length) {
+            add(str.subSequence(last, length))
+        }
+    }
+    return split
 }
