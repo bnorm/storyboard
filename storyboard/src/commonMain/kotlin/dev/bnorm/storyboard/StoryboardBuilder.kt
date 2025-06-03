cb27502e64ad9d009b6da00fa11c2aad8a407f0c
Brian Norman <brian.norman@jetbrains.com>
2025-06-03 14:59:57 -0500

    Remove StoryboardBuilder extensions functions

--- storyboard/src/commonMain/kotlin/dev/bnorm/storyboard/StoryboardBuilder.kt
+++ storyboard/src/commonMain/kotlin/dev/bnorm/storyboard/StoryboardBuilder.kt
@@ -1,7 +1,5 @@
 package dev.bnorm.storyboard
 
-import kotlin.enums.enumEntries
-
 @DslMarker
 internal annotation class StoryboardBuilderDsl
 
@@ -25,42 +23,4 @@ public sealed interface StoryboardBuilder {
         require(stateCount >= 0) { "stateCount must be greater than or equal to 0" }
         return scene((0..<stateCount).toList(), enterTransition, exitTransition, content)
     }
-}
-
-@StoryboardBuilderDsl
-public fun <T> StoryboardBuilder.scene(
-    vararg states: T,
-    enterTransition: SceneEnterTransition = SceneEnterTransition.Default,
-    exitTransition: SceneExitTransition = SceneExitTransition.Default,
-    content: SceneContent<T>,
-): Scene<T> {
-    return scene(states.asList(), enterTransition, exitTransition, content)
-}
-
-@StoryboardBuilderDsl
-public inline fun <reified T : Enum<T>> StoryboardBuilder.sceneForEnum(
-    enterTransition: SceneEnterTransition = SceneEnterTransition.Default,
-    exitTransition: SceneExitTransition = SceneExitTransition.Default,
-    content: SceneContent<T>,
-): Scene<T> {
-    return scene(enumEntries<T>(), enterTransition, exitTransition, content)
-}
-
-@StoryboardBuilderDsl
-public fun StoryboardBuilder.sceneForBoolean(
-    enterTransition: SceneEnterTransition = SceneEnterTransition.Default,
-    exitTransition: SceneExitTransition = SceneExitTransition.Default,
-    content: SceneContent<Boolean>,
-): Scene<Boolean> {
-    val states = listOf(false, true)
-    return scene(states, enterTransition, exitTransition, content)
-}
-
-@StoryboardBuilderDsl
-public fun StoryboardBuilder.sceneForTransition(
-    enterTransition: SceneEnterTransition = SceneEnterTransition.Default,
-    exitTransition: SceneExitTransition = SceneExitTransition.Default,
-    content: SceneContent<Nothing>,
-): Scene<Nothing> {
-    return scene(emptyList(), enterTransition, exitTransition, content)
 }
