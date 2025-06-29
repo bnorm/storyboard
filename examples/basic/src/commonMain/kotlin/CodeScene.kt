82a7274d2a0b6b723fe3fb6d46898c91a77e4abe
Brian Norman <brian.norman@jetbrains.com>
2025-06-29 17:34:26 -0500

    fixup! Update to material3

--- examples/basic/src/commonMain/kotlin/CodeScene.kt
+++ examples/basic/src/commonMain/kotlin/CodeScene.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Text
@@ -84,7 +84,7 @@ fun StoryboardBuilder.CodeScene() = scene(
 ) {
     Column(Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)) {
         Header { Text("Code Scene") }
-        Divider(color = MaterialTheme.colorScheme.primary)
+        HorizontalDivider(color = MaterialTheme.colorScheme.primary)
         Body {
             Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
                 val currentState = transition.currentState.toState()
