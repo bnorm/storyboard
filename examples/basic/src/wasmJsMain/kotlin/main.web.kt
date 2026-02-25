2ecf29cdd3dae09e4af2a3d62553c66d6d0388d8
Brian Norman <brian.norman@jetbrains.com>
2026-02-25 16:11:27 -0600

    Fix examples for web

--- examples/basic/src/wasmJsMain/kotlin/main.web.kt
+++ examples/basic/src/wasmJsMain/kotlin/main.web.kt
@@ -1,7 +1,7 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.window.CanvasBasedWindow
+import androidx.compose.ui.window.ComposeViewport
 import dev.bnorm.storyboard.easel.WebEasel
 import kotlinx.browser.document
 import org.w3c.dom.HTMLCanvasElement
@@ -11,7 +11,7 @@ fun main() {
     val storyboard = createStoryboard()
     val element = document.getElementById("ComposeTarget") as HTMLCanvasElement
     element.focus() // Focus is required for keyboard navigation.
-    CanvasBasedWindow(canvasElementId = element.id, title = storyboard.title) {
+    ComposeViewport(viewportContainer = element) {
         MaterialTheme(colors = darkColors()) {
             WebEasel { storyboard }
         }
