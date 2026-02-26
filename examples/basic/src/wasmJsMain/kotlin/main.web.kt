423a5cc5db95ddde73b618b71880366d5c8bbda3
Brian Norman <brian@bnorm.com>
2026-02-26 16:04:11 -0600

    Fix web example rendering

--- examples/basic/src/wasmJsMain/kotlin/main.web.kt
+++ examples/basic/src/wasmJsMain/kotlin/main.web.kt
@@ -3,14 +3,10 @@ import androidx.compose.material.darkColors
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import dev.bnorm.storyboard.easel.WebEasel
-import kotlinx.browser.document
-import org.w3c.dom.HTMLCanvasElement
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
-    val element = document.getElementById("ComposeTarget") as HTMLCanvasElement
-    element.focus() // Focus is required for keyboard navigation.
-    ComposeViewport(viewportContainer = element) {
+    ComposeViewport("composeApp") {
         MaterialTheme(colors = darkColors()) {
             WebEasel { createStoryboard() }
         }
